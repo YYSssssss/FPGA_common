5a2387eaffb51ef9be948dd54770a72d638c8bc7
Jade <864486277@qq.com>
2024-01-15 22:41:28 +0800

    240115

---
+++ python/test1_gen_filelist.py
@@ -0,0 +1,38 @@
+import os
+
+# def test1_gen_filelist():
+#     current_path = os.getcwd()
+#     file_path = os.path.join(current_path,"/synplify/rd.txt")
+
+#     with open (file_path,'r') as obj:
+#         data = obj.read()
+
+#     with open('file_syn.v','w') as f2:
+#         f2.write("set" + " " + "\n")
+
+#         for line in data.readlines():
+#             line.strip()
+#             if line == '\n' or line.startswith('//'):
+#                 f2.write(line)
+#             else:
+#                 line = line[:-1]
+#                 f2.write("add_file")
+#     obj.close()
+#     f2.close()
+
+def testl_gen_filelist():
+    filetype_verilog =" -verilog "
+    filetype_systemverilog = " -verilog -vlog std sysv "
+    vobs_path = " /prj/chips/pvg/venus/yuyushan/jade venus 3/vobs"
+    need_delate_word ="+incdir+"
+    need_delate_word1 ="+incdir+"
+    need_save_word2 ="+define+FPGA"
+    header0 ="header files are defined in"
+    need_operate_words = [need_delate_word,need_delate_word1,need_save_word2]
+    current_path = os.getcwd()
+# file rtl = os.path.join(current path,"rsp sl top filelist fpga")file rtl = os.path.join("/prj/chips/pvg/venus/yuyushan/jade venus 3/vobs/ip/
+with open(file rtl,'r') as obj:data = obi.read()
+with open('file syn.v','w') as f2:
+
+if __name__ == "__main__":
+    test1_gen_filelist()
---
+++ python/提取ila_code信息/verilog.v
@@ -0,0 +1,19 @@
+// 模块实例化段落1
+ila_1 your_instance_name1 (
+    .clk(clk1), // input wire clk1
+    .probe0(probe0_1), // input wire [0:0]  probe0_1  
+    .probe1(probe1_1), // input wire [0:0]  probe1_1 
+    .probe2(probe2_1), // [0:0]  probe2_1 
+    .probe3(probe3_1), // input wire [0:0]  probe3_1 
+    .probe4(probe4_1) // input wire [31:0]  probe4_1
+);
+
+// 模块实例化段落2
+ila_1 your_instance_name2 (
+    .clk(clk2), // input wire clk2
+    .probe0(probe0_2), // input wire [0:0]  probe0_2  
+    .probe1(probe1_2), // input wire [0:0]  probe1_2 
+    .probe2(probe2_2), // input wire [0:0]  probe2_2 
+    .probe3(probe3_2), // input wire [0:0]  probe3_2 
+    .probe4(probe4_2) // input wire [31:0]  probe4_2
+);
